9231a69040dcd4b1d14d8409c74892266f2ebf7b
ayan0326 <zhangyuy1996@163.com>
2021-02-24 11:11:04 +0800

    第一次提交

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          首次页面内容
+        </a>
+        <p>
+          现在你就接到了那个网站问题的紧急电话，需要马上修补。有了 Git
+          ，我们就不需要同时发布这个补丁和 iss53
+          里作出的修改，也不需要在创建和发布该补丁到服务器之前花费大力气来复原这些修改。唯一需要的仅仅是切换回master
+          分支。
+          不过在此之前，留心你的暂存区或者工作目录里，那些还没有提交的修改，它会和你即将检出的分支产生冲突从而阻止
+          Git
+          为你切换分支。切换分支的时候最好保持一个清洁的工作区域。稍后会介绍几个绕过这种问题的办法（分别叫做
+          stashing 和 commit
+          amending）。目前已经提交了所有的修改，所以接下来可以正常转换到master
+          分支：
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
